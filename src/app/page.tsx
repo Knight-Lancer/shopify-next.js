7e00849c9ebd9f00c829ccec38a812eed60b0f71
Knight-Lancer <worksup782@gmail.com>
2026-08-02 01:01:52 -0400

    complete Shopify Store

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import AllCollections from "@/components/view/AllCollections";
+
 export default function Home() {
   return (
-    <main className="flex items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold">Hello World</h1>
-    </main>
+    <div>
+      <AllCollections />
+    </div>
   );
 }
